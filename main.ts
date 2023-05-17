e5e90adca90641158f90e60067870e23b4a682bf
huntfeng <spacecurvature@gmail.com>
2023-05-16 21:43:31 -0600

    simple memo plugin

--- main.ts
+++ main.ts
@@ -1,56 +1,60 @@
-interface Button {
-  icon: string;
-  onClick: () => void;
-}
-interface View {
-  mount: (parent: HTMLElement) => void;
-}
+import { Plugin, Button, NoteType, Controller } from "./plugin";
 
-class MyPlugin {
-  buttons: Button[] = [];
-  views: View[] = [];
-
+class MyPlugin extends Plugin {
   enable() {
     let button = {
-      icon: "books",
-      onClick: () => {
-        console.log("btn cliced");
-      },
+      icon: "assignment",
+      tooltip: "Memo",
+      onClick: this.openMemo,
     } as Button;
-    let view = {
-      mount: (parent: HTMLElement) => {
-        if (!parent) return;
-
-        let h1 = document.createElement("h1");
-        h1.innerHTML = "H1";
-        h1.onclick = (e: MouseEvent) => {
-          console.log("h1 clicked");
-        };
-
-        let h2 = document.createElement("h2");
-        h2.innerHTML = "H2";
-        h2.onclick = (e: MouseEvent) => {
-          console.log("h2 clicked");
-        };
-
-        parent.append(h1, h2);
-      },
-    } as View;
-    this.registerButton(button);
-
-    this.registerLeftMenuView(view);
+    this.addRibbonBtn(button);
   }
 
   disable() {
-    console.log("good bye");
+    this.controller.layout.closePage("memo");
   }
 
-  registerButton(btn: Button) {
-    this.buttons.push(btn);
+  /**
+   * Create Memo in database and its actual file in disk
+   * @returns success
+   */
+  async createMemo(): Promise<boolean> {
+    try {
+      let folder = this.controller.path.join(
+        await this.getStoragePath(),
+        "memo"
+      );
+      if (!this.controller.fs.existsSync(folder))
+        this.controller.fs.mkdirSync(folder);
+      let memoPath = this.controller.path.join(folder, "memo.md");
+
+      let memo = {
+        _id: "memo", // id in pouchdb for finding
+        _rev: "", // rev in pouch for version keep
+        dataType: "note", // research helper will open note page
+        type: NoteType.MARKDOWN, // will open markdown note page
+        label: "Memo", // label on the window tab
+        path: memoPath, // markdown file path (use absolute path)
+      };
+      await this.controller.db.put(memo); // put into database
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 
-  registerLeftMenuView(view: View) {
-    this.views.push(view);
+  async openMemo() {
+    // If memo exists (create it if not), then open the page for it.
+    // No need to worry about save/load,
+    // since the path is in the note object,
+    // the note page will take care of the .md file
+    try {
+      await this.controller.db.get("memo");
+    } catch (error) {
+      await this.createMemo();
+    }
+    this.controller.layout.openPage("memo");
   }
 }
 
